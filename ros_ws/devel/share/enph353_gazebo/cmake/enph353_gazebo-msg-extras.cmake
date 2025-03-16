4be83dec800b7f0bfb8e06c975b38441b82c8158
Hannah Cha <hannahviccha@gmail,com>
2025-03-16 18:23:46 +0000

    added ros_ws

---
+++ ros_ws/devel/share/enph353_gazebo/cmake/enph353_gazebo-msg-extras.cmake
@@ -0,0 +1,2 @@
+set(enph353_gazebo_MESSAGE_FILES "")
+set(enph353_gazebo_SERVICE_FILES "/home/hannahcha/ros_ws/src/2024_competition/enph353/enph353_gazebo/srv/GetLegalPlates.srv;/home/hannahcha/ros_ws/src/2024_competition/enph353/enph353_gazebo/srv/SubmitPlate.srv")
